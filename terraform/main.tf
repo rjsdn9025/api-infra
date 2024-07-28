1ad549300eea3eb65c650d5677bc77f343eed78f
rjsdn9025 <rjsdn9025@naver.com>
2024-07-28 19:58:12 +0900

    Add mysql-pv, mysql-pvc

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,6 +64,11 @@ resource "kubernetes_deployment" "mysql" {
           }
 
           volume_mount {
+            name       = "mysql-data"
+            mount_path = "/var/lib/mysql"
+          }
+
+          volume_mount {
             name       = "mysql-initdb"
             mount_path = "/docker-entrypoint-initdb.d"
           }
@@ -75,8 +80,63 @@ resource "kubernetes_deployment" "mysql" {
             name = kubernetes_config_map.mysql_init.metadata[0].name
           }
         }
+
+        volume {
+          name = "mysql-data"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.mysql_pvc.metadata[0].name
+          }
+        }
       }
     }
+  }
+}
+
+resource "kubernetes_config_map" "mysql_init" {
+  metadata {
+    name      = "mysql-initdb-config"
+    namespace = kubernetes_namespace.example.metadata[0].name
+  }
+
+  data = {
+    "init.sql" = file("init.sql")
+  }
+}
+
+resource "kubernetes_persistent_volume" "mysql_pv" {
+  metadata {
+    name = "mysql-pv"
+  }
+  spec {
+    volume_mode = "Filesystem"
+    capacity = {
+      storage = "1Gi"
+    }
+    persistent_volume_source {
+      host_path {
+        path = "/mysql/mysql_data"
+      }
+    }
+    access_modes = ["ReadWriteOnce"]
+    persistent_volume_reclaim_policy = "Retain"
+    storage_class_name = "standard"
+    
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "mysql_pvc" {
+  metadata {
+    name      = "mysql-pvc"
+    namespace = "example"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+    volume_name = kubernetes_persistent_volume.mysql_pv.metadata[0].name
   }
 }
 
